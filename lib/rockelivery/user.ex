e09f4dcaa7cfa38e4dfc3baa234d8162d30a0d5c
Abner <abnerchoba@hotmail.com>
2022-12-08 20:11:53 -0300

    feat: create users

---
+++ lib/rockelivery/user.ex
@@ -0,0 +1,42 @@
+defmodule Rockelivery.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  @required_params [:name, :age, :address, :cep, :cpf, :email, :password]
+
+  schema "users" do
+    field :age, :integer
+    field :name, :string
+    field :address, :string
+    field :cep, :string
+    field :cpf, :string
+    field :email, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps()
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_length(:password, min: 6)
+    |> validate_length(:cep, is: 8)
+    |> validate_length(:cpf, is: 11)
+    |> validate_number(:age, greater_than_or_equal_to: 18)
+    |> unique_constraint([:email])
+    |> unique_constraint([:cpf])
+    |> put_password_hash()
+  end
+
+  defp put_password_hash(%Changeset{valid?: true, changes: %{password: password}} = user_cs) do
+    change(user_cs, Pbkdf2.add_hash(password))
+  end
+
+  defp put_password_hash(user_cs), do: user_cs
+end
